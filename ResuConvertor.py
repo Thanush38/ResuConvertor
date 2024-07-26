4c32e327298b29aff22bc8d2e00999d7a9800f4e
thanush <thanush38@outlook.com>
2024-07-26 10:59:01 -0400

    working on converotr

--- ResuConvertor.py
+++ ResuConvertor.py
@@ -1,4 +1,43 @@
 import os
 import sys
+from dotenv import load_dotenv
+from docx2pdf import convert
 
-def convertor():
+
+def getName(data):
+    load_dotenv()
+    if(data=="-r"):
+        resumeName = os.getenv("RESUME_NAME")
+        return resumeName
+    elif(data=="-c"):
+        coverName = os.getenv("COVER_LETTER_NAME")
+        return coverName
+    elif (data=="-a"):
+        resumeName = os.getenv("RESUME_NAME")
+        coverName = os.getenv("COVER_LETTER_NAME")
+        return resumeName, coverName
+    else: 
+        return "Invalid Argument"
+
+
+
+def mainFunc():
+    for i in sys.argv:
+        if(i=="-r"):
+            resumeName = getName(i)
+            ConvertToPDF(resumeName)
+        elif(i=="-c"):
+            coverName = getName(i)
+            ConvertToPDF(coverName)
+        elif(i=="-a"):
+            resumeName, coverName = getName(i)
+            ConvertToPDF(resumeName)
+            ConvertToPDF(coverName)
+        else:
+            print("Invalid Argument")
+
+
+def ConvertToPDF(fileName):
+    convert(fileName, fileName + ".pdf")
+    
+mainFunc()
